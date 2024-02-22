843698db2b9066f73ebd754c5a123a3d85b3b930
Gavin Burns <burnsg@wit.edu>
2024-02-22 23:21:14 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,34 +1,55 @@
-// create web server
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var url = require('url');
+// Create web server
+// Run: node comments.js
+// Test: http://localhost:3000
 
-// create a web server
-http.createServer(function(req, res) {
-  var pathname = url.parse(req.url).pathname;
-  var filepath = path.join(__dirname, pathname);
-  console.log('pathname: ', pathname);
-  console.log('filepath: ', filepath);
-  fs.exists(filepath, function(exists) {
-    if (exists) {
-      fs.readFile(filepath, function(err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      });
-    } else {
-      res.writeHead(404, { 'Content-Type': 'text/html' });
-      res.end('404 Not Found');
-    }
-  });
-}).listen(3000, function() {
-  console.log('Server is listening on port 3000');
-});
-
-// create a web server using express
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname));
-app.listen(3001, function() {
-  console.log('Server is listening on port 3001');
+
+// Set up the database
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/comments');
+
+// Set up the schema
+var commentSchema = mongoose.Schema({
+    name: String,
+    comment: String
+});
+
+// Set up the model
+var Comment = mongoose.model('Comment', commentSchema);
+
+// Set up the body parser
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set up the ejs view engine
+app.set('view engine', 'ejs');
+
+// Set up the public folder
+app.use(express.static('public'));
+
+// Set up the routes
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/getComments', function(req, res) {
+    Comment.find({}, function(err, comments) {
+        if (err) throw err;
+        res.send(comments);
+    });
+});
+
+app.post('/addComment', function(req, res) {
+    var newComment = new Comment(req.body);
+    newComment.save(function(err, data) {
+        if (err) throw err;
+        res.send(data);
+    });
+});
+
+// Set up the server
+app.listen(3000, function() {
+    console.log('Server running on port 3000');
 });
